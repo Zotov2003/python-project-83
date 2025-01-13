eec3ae520b89616d52577cfb036d7526fd03b07c
Zotov2003 <baiter159159@gmail.com>
2025-01-13 20:59:22 +0500

    clear

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,17 +1,25 @@
 import os
+from urllib.parse import urlparse
+
 from dotenv import load_dotenv
 from flask import (
     Flask,
     flash,
+    get_flashed_messages,
+    redirect,
     render_template,
     request,
     url_for,
-    get_flashed_messages,
-    redirect,
-    jsonify
 )
-from urllib.parse import urlparse
-from page_analyzer.db import DatabaseRepository
+
+from page_analyzer.db import (
+    add_check_to_db,
+    add_url_to_db,
+    get_checks_desc,
+    get_url_by_id,
+    get_url_by_name,
+    get_urls_with_latest_check,
+)
 from page_analyzer.helpers import fetch_url_data
 from page_analyzer.tasks import async_check_all_urls
 from page_analyzer.url_validator import validate
@@ -21,86 +29,92 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-repo = DatabaseRepository(os.getenv('DATABASE_URL'))
-
-def normalize_url(url):
-    parsed_url = urlparse(url)
-    return f"{parsed_url.scheme}://{parsed_url.netloc}"
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+
 @app.get('/')
 def page_analyzer():
-    return render_template('index.html', flashed_messages=get_flashed_messages(with_categories=True))
+    message = get_flashed_messages(with_categories=True)
+
+    return render_template('index.html', message=message)
+
 
 @app.post('/urls')
 def add_url():
     new_url = request.form.get('url')
-    normalized_url = normalize_url(new_url)
 
     error = validate(new_url)
     if error:
-        flash(error, 'danger')
-        return render_template('index.html'), 422
+        flash(f'{error}', 'danger')
+        message = get_flashed_messages(with_categories=True)
+        return render_template('index.html', message=message), 422
 
-    url_data = repo.get_url_by_name(normalized_url)
+    parsed_url = urlparse(new_url)
+    normal_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+
+    url_data = get_url_by_name(normal_url)
     if url_data:
         flash('Страница уже существует', 'primary')
-        return redirect(url_for('page_analyzer'))
-    else:
-        try:
-            repo.add_url_to_db(normalized_url)
-            new_url_data = repo.get_url_by_name(normalized_url)
-            flash('Страница успешно добавлена', 'success')
-        except Exception as e:
-            flash(f'Ошибка при добавлении URL: {str(e)}', 'danger')
-            return redirect(url_for('page_analyzer'))
+        return redirect(url_for('show_url', id=url_data.id))
 
-    return redirect(url_for('page_analyzer'))
+    add_url_to_db(normal_url)
+    new_url_data = get_url_by_name(normal_url)
+
+    flash('Страница успешно добавлена', 'success')
+    return redirect(url_for('show_url', id=new_url_data.id))
 
 
 @app.get('/urls')
 def show_all_urls():
-    all_urls = repo.get_urls_with_latest_check()
-    return render_template('urls.html', all_urls=all_urls, flashed_messages=get_flashed_messages(with_categories=True))
+    all_urls = get_urls_with_latest_check()
+    message = get_flashed_messages(with_categories=True)
+    return render_template('urls.html', all_urls=all_urls, message=message)
+
 
 @app.post('/urls/checks')
 def check_all_urls():
     async_check_all_urls.delay()
     flash('Процесс проверки всех страниц запушен', 'success')
+
     return redirect(url_for('show_all_urls'))
+
 
 @app.get('/urls/<int:id>')
 def show_url(id):
-    url_data = repo.get_url_by_id(id)
+    url_data = get_url_by_id(id)
     if not url_data:
         return render_template('404.html'), 404
 
-    all_checks = repo.get_checks_desc(id)
+    all_checks = get_checks_desc(id)
+    message = get_flashed_messages(with_categories=True)
     return render_template(
         'url.html',
         url_data=url_data,
         all_checks=all_checks,
-        flashed_messages=get_flashed_messages(with_categories=True)
+        message=message
     )
+
 
 @app.post('/urls/<id>/checks')
 def add_check(id):
-    url = repo.get_url_by_id(id)
+    url = get_url_by_id(id)
     if not url:
         return render_template('404.html'), 404
 
     status_code, page_data = fetch_url_data(url[0].name)
+
     if status_code == 0:
         flash('Произошла ошибка при проверке', 'danger')
     else:
-        repo.add_check_to_db(id, status_code, page_data)
+        add_check_to_db(id, status_code, page_data)
         flash('Страница успешно проверена', 'success')
 
     return redirect(url_for('show_url', id=id))
--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,77 +1,79 @@
+import os
+
 import psycopg2
+from dotenv import load_dotenv
 from psycopg2.extras import NamedTupleCursor
-from contextlib import contextmanager
 
-class DatabaseRepository:
-    def __init__(self, db_url):
-        self.db_url = db_url
+load_dotenv()
 
-    @contextmanager
-    def connection(self):
-        conn = psycopg2.connect(self.db_url, cursor_factory=NamedTupleCursor)
-        try:
-            yield conn
-        finally:
-            conn.close()
+DATABASE_URL = os.getenv('DATABASE_URL')
 
-    def fetch_all(self, query, values=()):
-        with self.connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute(query, values)
-                return cur.fetchall()
 
-    def execute(self, query, values=()):
-        with self.connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute(query, values)
-                conn.commit()
+def get_connection():
+    return psycopg2.connect(DATABASE_URL)
 
-    def add_url_to_db(self, url):
-        self.execute("INSERT INTO urls (name) VALUES (%s)", (url,))
 
-    def get_url_by_name(self, url):
-        results = self.fetch_all("SELECT * FROM urls WHERE name = %s", (url,))
-        if results:
-            return results[0]
-        else:
-            return None
+def fetch_all(query, values=()):
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+            cur.execute(query, values)
+            return cur.fetchall()
 
-    def get_url_by_id(self, url_id):
-        return self.fetch_all("SELECT * FROM urls WHERE id = %s", (url_id,))[0]
 
-    def add_check_to_db(self, url_id, status_code, page_data):
-        self.execute(
-            """
-            INSERT INTO url_checks (
+def add_url_to_db(url):
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute("INSERT INTO urls (name) VALUES (%s)", (url,))
+            conn.commit()
+
+
+def get_url_by_name(url):
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+            cur.execute("SELECT * FROM urls WHERE name = %s", (url,))
+            return cur.fetchone()
+
+
+def get_url_by_id(url_id):
+    query = "SELECT * FROM urls WHERE id = %s"
+    return fetch_all(query, (url_id,))
+
+
+def add_check_to_db(url_id, status_code, page_data):
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO url_checks (
                 url_id, status_code, h1, title, description)
-            VALUES (%s, %s, %s, %s, %s)
-            """,
-            (url_id, status_code,
-             page_data['h1'], page_data['title'], page_data['description']),
-        )
+                VALUES (%s, %s, %s, %s, %s)
+                """,
+                (url_id, status_code,
+                 page_data['h1'], page_data['title'], page_data['description']),
+            )
+            conn.commit()
 
-    def get_urls_with_latest_check(self):
-        return self.fetch_all("""
-               SELECT urls.id, urls.name,
-               COALESCE(url_checks.status_code::text, '') as status_code,
-               COALESCE(MAX(url_checks.created_at)::text, '') as latest_check
-               FROM urls
-               LEFT JOIN url_checks ON urls.id = url_checks.url_id
-               GROUP BY urls.id, url_checks.status_code
-               ORDER BY urls.id DESC
-           """)
 
-    def get_checks_desc(self, url_id):
-        return self.fetch_all("""
-               SELECT id, status_code, COALESCE(h1, '') as h1,
-               COALESCE(title, '') as title, COALESCE(description, '') as description,
-               created_at::text
-               FROM url_checks
-               WHERE url_id = %s
-               ORDER BY id DESC
-           """, (url_id,))
+def get_urls_with_latest_check():
+    query = """
+        SELECT urls.id, urls.name,
+        COALESCE(url_checks.status_code::text, '') as status_code,
+        COALESCE(MAX(url_checks.created_at)::text, '') as latest_check
+        FROM urls
+        LEFT JOIN url_checks ON urls.id = url_checks.url_id
+        GROUP BY urls.id, url_checks.status_code
+        ORDER BY urls.id DESC
+    """
+    return fetch_all(query)
 
-    def add_url_to_db_with_returning(self, url):
-        query = "INSERT INTO urls (name) VALUES (%s) RETURNING *"
-        result = self.fetch_all(query, (url,))
-        return result[0] if result else None
+
+def get_checks_desc(url_id):
+    query = """
+        SELECT id, status_code, COALESCE(h1, '') as h1,
+        COALESCE(title, '') as title, COALESCE(description, '') as description,
+        created_at::text
+        FROM url_checks
+        WHERE url_id = %s
+        ORDER BY id DESC
+    """
+    return fetch_all(query, (url_id,))
