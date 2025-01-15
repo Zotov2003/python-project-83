fd56eff3301d7240a9513267480bd874803fcdb2
Zotov2003 <baiter159159@gmail.com>
2025-01-15 20:28:51 +0500

    res2

--- page_analyzer/helpers.py
+++ page_analyzer/helpers.py
@@ -1,16 +1,4 @@
-import requestsimport requests
-
-from page_analyzer.html_parser import parse_page
-
-
-def fetch_url_data(url):
-    try:
-        response = requests.get(url, timeout=10)
-        response.raise_for_status()
-        return response.status_code, parse_page(response.text)
-    except requests.exceptions.RequestException as e:
-        print(f"Сетевая ошибка: {e}")
-        return 0, {'title': '', 'h1': '', 'description': 'Ошибка сети'}
+import requests
 
 from page_analyzer.html_parser import parse_page
 
