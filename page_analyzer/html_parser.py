e452348f7e56c1bc461382870b55dc0c9ee3e08d
Zotov2003 <baiter159159@gmail.com>
2025-01-18 11:51:00 +0500

    update 1

--- page_analyzer/html_parser.py
+++ page_analyzer/html_parser.py
@@ -1,15 +1,17 @@
 from bs4 import BeautifulSoup
 
-
 def parse_page(response_text):
     html_data = BeautifulSoup(response_text, 'html.parser')
-    page_data = {'title': html_data.title.string if html_data.title else None,
-                 'h1': html_data.h1.string if html_data.h1 else None}
+    title = html_data.title.string if html_data.title else None
+    h1 = html_data.h1.string if html_data.h1 else None
 
     description = html_data.find('meta', {'name': 'description'})
-    if description:
-        description = description.get('content')
+    description_content = description.get('content') if description else None
 
-    page_data['description'] = description
+    page_data = {
+        'title': title,
+        'h1': h1,
+        'description': description_content
+    }
 
     return page_data
