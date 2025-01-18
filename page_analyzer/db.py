3eeeeb5633a7b53e4624c246736c342b9c19e089
Zotov2003 <baiter159159@gmail.com>
2025-01-18 12:22:51 +0500

    3

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,5 +1,4 @@
 import os
-
 import psycopg2
 from dotenv import load_dotenv
 from psycopg2.extras import NamedTupleCursor
@@ -9,50 +8,59 @@ load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 
-def get_connection():
-    return psycopg2.connect(DATABASE_URL)
+class DatabaseRepository:
+    def __init__(self):
+        self.conn = None
 
+    def __enter__(self):
+        self.conn = psycopg2.connect(DATABASE_URL)
+        return self
 
-def fetch_all(query, values=()):
-    with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.conn:
+            self.conn.close()
+
+    def cursor(self, cursor_factory=NamedTupleCursor):
+        return self.conn.cursor(cursor_factory=cursor_factory)
+
+    def fetch_all(self, query, values=()):
+        with self.cursor() as cur:
             cur.execute(query, values)
             return cur.fetchall()
 
+    def execute(self, query, values=()):
+        with self.cursor() as cur:
+            cur.execute(query, values)
+            self.conn.commit()
 
 def add_url_to_db(url):
-    with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor() as cur:
-            cur.execute("INSERT INTO urls (name) VALUES (%s)", (url,))
-            conn.commit()
-
+    with DatabaseRepository() as repo:
+        repo.execute("INSERT INTO urls (name) VALUES (%s)", (url,))
 
 def get_url_by_name(url):
-    with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-            cur.execute("SELECT * FROM urls WHERE name = %s", (url,))
-            return cur.fetchone()
-
+    with DatabaseRepository() as repo:
+        results = repo.fetch_all("SELECT * FROM urls WHERE name = %s", (url,))
+        if results:
+            return results[0]
+        else:
+            return None
 
 def get_url_by_id(url_id):
     query = "SELECT * FROM urls WHERE id = %s"
-    return fetch_all(query, (url_id,))
-
+    with DatabaseRepository() as repo:
+        return repo.fetch_all(query, (url_id,))[0]
 
 def add_check_to_db(url_id, status_code, page_data):
-    with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                """
-                INSERT INTO url_checks (
-                url_id, status_code, h1, title, description)
-                VALUES (%s, %s, %s, %s, %s)
-                """,
-                (url_id, status_code,
-                 page_data['h1'], page_data['title'], page_data['description']),
-            )
-            conn.commit()
-
+    with DatabaseRepository() as repo:
+        repo.execute(
+            """
+            INSERT INTO url_checks (
+            url_id, status_code, h1, title, description)
+            VALUES (%s, %s, %s, %s, %s)
+            """,
+            (url_id, status_code,
+             page_data['h1'], page_data['title'], page_data['description']),
+        )
 
 def get_urls_with_latest_check():
     query = """
@@ -64,8 +72,8 @@ def get_urls_with_latest_check():
         GROUP BY urls.id, url_checks.status_code
         ORDER BY urls.id DESC
     """
-    return fetch_all(query)
-
+    with DatabaseRepository() as repo:
+        return repo.fetch_all(query)
 
 def get_checks_desc(url_id):
     query = """
@@ -76,4 +84,5 @@ def get_checks_desc(url_id):
         WHERE url_id = %s
         ORDER BY id DESC
     """
-    return fetch_all(query, (url_id,))
+    with DatabaseRepository() as repo:
+        return repo.fetch_all(query, (url_id,))
