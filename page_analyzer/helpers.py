9766d72e97df775d36a36985cedbe0577732adfe
Zotov2003 <baiter159159@gmail.com>
2025-01-22 19:25:15 +0500

    1.1

--- page_analyzer/helpers.py
+++ page_analyzer/helpers.py
@@ -16,7 +16,8 @@ def parse_html_data(html_content):
         soup = BeautifulSoup(html_content, 'html.parser')
         title = soup.title.string if soup.title else ''
         h1 = soup.find('h1').string if soup.find('h1') else ''
-        description = soup.find('meta', attrs={'name': 'description'})['content'] \
+        description = \
+            soup.find('meta', attrs={'name': 'description'})['content'] \
             if soup.find('meta', attrs={'name': 'description'}) else ''
         return {'title': title, 'h1': h1, 'description': description}
     except AttributeError as e:
